c2a643e0598f1e46106b9b9acbb548eccbe86732
artk0st <art_k0st@icloud.com>
2023-03-26 21:54:08 +0300

    add GORM Automigrations

--- internal/models/root_domain.go
+++ internal/models/root_domain.go
@@ -4,7 +4,7 @@ import "time"
 
 type RootDomain struct {
 	ID        uint      `json:"id" gorm:"primaryKey;unique;autoIncrement"`
-	Name      string    `json:"name" gorm:"unique"`
+	Name      string    `json:"name" gorm:"index:idx_name,unique"`
 	Type      string    `json:"type"`
 	TLD       string    `json:"tld"`
 	Source    string    `json:"source"`
