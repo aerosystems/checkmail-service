7d65692f74b6076079d51be0be0009ac77956186
aerosystems <art_k0st@icloud.com>
2025-02-09 13:58:50 +0200

    fix count method

--- internal/ports/http/handler_domain_count.go
+++ internal/ports/http/handler_domain_count.go
@@ -7,15 +7,19 @@ import (
 )
 
 type CountResponse struct {
-	Count map[string]int `json:"count"`
+	Whitelist int `json:"whitelist"`
+	Blacklist int `json:"blacklist"`
 }
 
-func responseFromModel(m map[models.Type]int) map[string]int {
-	countMap := make(map[string]int)
-	for k, v := range m {
-		countMap[k.String()] = v
+func responseFromModel(m map[models.Type]int) *CountResponse {
+	var count CountResponse
+	if v, ok := m[models.BlacklistType]; ok {
+		count.Blacklist = v
 	}
-	return countMap
+	if v, ok := m[models.WhitelistType]; ok {
+		count.Whitelist = v
+	}
+	return &count
 }
 
 // Count godoc
@@ -31,5 +35,5 @@ func (h Handler) Count(c echo.Context) error {
 	if err != nil {
 		return err
 	}
-	return c.JSON(http.StatusOK, CountResponse{responseFromModel(count)})
+	return c.JSON(http.StatusOK, responseFromModel(count))
 }
