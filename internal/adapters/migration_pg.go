f02db97527e1823f22e1d90f25b9cac7fa8f6643
aerosystems <art_k0st@icloud.com>
2025-02-12 01:56:49 +0200

    change enum

--- internal/adapters/migration_pg.go
+++ internal/adapters/migration_pg.go
@@ -9,7 +9,7 @@ func AutoMigrateGORM(db *gorm.DB) error {
 	if err := db.Exec(`
 		DO $$ BEGIN
 			IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'domain_type') THEN
-				CREATE TYPE domain_type AS ENUM ('unknown', 'blacklist', 'whitelist');
+				CREATE TYPE domain_type AS ENUM ('undefined', 'blacklist', 'whitelist');
 			END IF;
 			IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'match_type') THEN
 				CREATE TYPE match_type AS ENUM ('prefix', 'suffix', 'contains', 'equals');
